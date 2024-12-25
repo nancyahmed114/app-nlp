b8eb5a76d874f49eae47cc993fcd4f038431223d
nancyahmed114 <nancyahmed1104@yahoo.com>
2024-12-25 17:43:33 +0200

    Add files via upload

---
+++ nlpp/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import sklearn
+import helper
+import pickle
+import nltk
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
+model=pickle.load(open("model/model.pkl",'rb'))
+vectorizer=pickle.load(open("model/vectorizer.pkl",'rb'))
+
+st.text("sentiment analysis")
+state = st.button("predict")
+text = st.text_input("please enter your review")
+
+token = helper.preprocessing_step(text)
+vectorized_data = vectorizer.transform([token])
+prediction = model.predict(vectorized_data)
+
+if state :
+    st.text(prediction)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nlpp/helper.py
@@ -0,0 +1,20 @@
+import re   ### for regex expressions
+from nltk.corpus import stopwords ### NLTK for NLP taskss
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.tree import DecisionTreeClassifier
+
+
+def preprocessing_step(text):
+    text = text.lower()
+    ### Remove any special charchter
+    text =re.sub('[^a-zA-Z]',' ',text)
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word not in stop_words]
+    stemmer = PorterStemmer()
+    stemmed_tokens = [stemmer.stem(word) for word in filtered_tokens]
+    stemmed_tokens = ' '.join(stemmed_tokens)
+    return stemmed_tokens
